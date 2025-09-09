768f48ab942adfca82b0cc34f9cf5c72f6f5e8ed
Herbadableh <herbadableh@gmail.com>
2025-09-09 12:07:50 -0400

    Extraction + Fix
    -Fixed crash when block is placed due to calling the facing before it could load
    -Implemented Time Sand extraction functionality
    
    TODO: way to reverse the process by using a redstone signal

--- src/main/java/itemtestagain/block/TestTileEntity.java
+++ src/main/java/itemtestagain/block/TestTileEntity.java
@@ -4,6 +4,7 @@ package itemtestagain.block;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ITickable;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidTank;
@@ -12,12 +13,20 @@ import net.minecraftforge.fluids.IFluidTank;
 import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
 import net.minecraftforge.fluids.capability.IFluidHandler;
 import itemtestagain.block.BlockTest;
-
-public class TestTileEntity extends TileEntity{
+import lumaceon.mods.clockworkphase.block.tileentity.ITimeSandTile;
+import static lumaceon.mods.clockworkphase.init.ModFluids.timeSand;
+/*implements ITickable*/
+public class TestTileEntity extends TileEntity implements ITickable{
 
     public FluidTank tank = new FluidTank(10000);
-    //private int counter = 0;
-    private final EnumFacing front = world.getBlockState(pos).getValue(BlockTest.FACING);
+    private int wait = 20;
+
+    //private final EnumFacing front = world.getBlockState(pos).getValue(BlockTest.FACING);
+    //ends up becoming null when first placed in world, cannot load
+    //you have to put it in individual functions, not as single variable
+
+
+
 
     /*public int decrement() {
         counter--;
@@ -32,19 +41,36 @@ public class TestTileEntity extends TileEntity{
     }
     */
 
-    /*
+
     @Override
     public void update() {
+
         if(!world.isRemote)
         {
-            TileEntity te = world.getTileEntity(pos.offset(front));
-            if(te != null && te)
+            wait--;
+            if(wait <= 0)
+            {wait = 20;//timer reset
+                EnumFacing front = world.getBlockState(pos).getValue(BlockTest.FACING);
+                    TileEntity te = world.getTileEntity(pos.offset(front));//tile entity directly infront of the machine
+                    if (te instanceof ITimeSandTile) {
+                        //either the amount of space left in the tank, or how much sand is in the machine, whichever is smaller.
+                        int sandPile = Math.min((tank.getCapacity() - tank.getFluidAmount()), ((ITimeSandTile) te).getTimeSand());
+                        if ((tank.getFluid() == null || tank.getFluid().getFluid() == timeSand) && sandPile >= 0)//if the tank is empty or has time sand and isn't full:
+                        {
+                            int sandIn = ((ITimeSandTile) te).removeTimeSand(sandPile);//checked the code, time sand IS equal to mB.
+                            FluidStack sand = new FluidStack(timeSand, sandIn);
+                            tank.fillInternal(sand, true);
+                        }
+                    }
+            }
         }
+
     }
-*/
+
+
     @Override//                                             v the face trying to be accessed
     public boolean hasCapability(Capability<?> capability, EnumFacing facing) {
-        if (capability == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY && facing == front.getOpposite()) {
+        if (capability == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY && facing == world.getBlockState(pos).getValue(BlockTest.FACING).getOpposite()) {
             return true;
         }
         return super.hasCapability(capability, facing);
@@ -52,12 +78,11 @@ public class TestTileEntity extends TileEntity{
 
     @Override
     public <T> T getCapability(Capability<T> capability, EnumFacing facing) {
-        if (capability == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY && facing == front.getOpposite()) {
+        if (capability == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY && facing == world.getBlockState(pos).getValue(BlockTest.FACING).getOpposite()) {
             return CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY.cast(tank);
         }
         return super.getCapability(capability, facing);
     }
-
 
 
 
